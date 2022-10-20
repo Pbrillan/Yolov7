fad6d4a0436519da72781b00a7ef7f59d91c7495
Pbrillan <73187568+Pbrillan@users.noreply.github.com>
2022-10-20 19:14:18 -0300

    Add files via upload

--- detect.py
+++ detect.py
@@ -84,8 +84,7 @@ def detect(save_img=False):
 
         # Inference
         t1 = time_synchronized()
-        with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
-            pred = model(img, augment=opt.augment)[0]
+        pred = model(img, augment=opt.augment)[0]
         t2 = time_synchronized()
 
         # Apply NMS
@@ -117,7 +116,9 @@ def detect(save_img=False):
                     s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
                 # Write results
+                i=0
                 for *xyxy, conf, cls in reversed(det):
+                    i = i+1
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
@@ -126,7 +127,11 @@ def detect(save_img=False):
 
                     if save_img or view_img:  # Add bbox to image
                         label = f'{names[int(cls)]} {conf:.2f}'
-                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+                        #plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+                        cord = (torch.tensor(xyxy).view(1, 4) ).view(-1).tolist()
+                        #print(int(cord[1]),int(cord[3]),int(cord[0]),int(cord[2]))
+                        im1= im0[int(cord[1]*0.9):int(cord[3]*1.1),int(cord[0]*0.9):int(cord[2]*1.1)]
+                        cv2.imwrite(save_path+str(i)+".jpg", im1)
 
             # Print time (inference + NMS)
             print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
